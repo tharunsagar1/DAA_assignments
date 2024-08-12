4dcb41040fbc9498276113d0a3632df64860edbe
tharunsagar1 <158567083+tharunsagar1@users.noreply.github.com>
2024-08-12 21:50:04 +0530

    Update price.c

--- price.c
+++ price.c
@@ -1,3 +1,11 @@
+/*Imagine you are working for a large online marketplace like Amazon or eBay. One
+of the critical functionalities of such platforms is to display products to customers in
+a way that is relevant, helpful, and easy to navigate. Customers can browse through
+thousands or even millions of products, and Many users prefer to see products sorted
+by price, either in ascending or descending order. This allows them to find the
+cheapest or most expensive products within their budget. So, implement an
+application to arrange the products based on price*/
+
 #include <stdio.h>
 
 void price(int arr[], int l) {
