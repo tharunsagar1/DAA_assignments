4682b5750c4308ad167820804ff6a71d9492a691
tharunsagar1 <158567083+tharunsagar1@users.noreply.github.com>
2024-08-12 21:52:03 +0530

    Update retail_product_id.c

--- retail_product_id.c
+++ retail_product_id.c
@@ -1,3 +1,12 @@
+
+/*Imagine you are working for a retail store that sells a wide variety of products. The
+store has a vast inventory with thousands of items, and it's becoming challenging for
+the employees to manage and locate products efficiently. Customers often ask for
+specific items, and employees need to find them quickly. The sorting program's
+primary goal is to organize the products in the inventory systematically, allowing for
+faster and easier access to items when needed.
+Implement an efficient sorting algorithm to arrange the products based on product
+IDs*/
 #include <stdio.h>
 
 void product_id_sort(int arr[], int l) {
